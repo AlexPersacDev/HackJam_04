403856b648e294da2860bf12f8a07f4f2f33852e
Alex Pereira Sacristan <alexpersac01@gmail.com>
2024-05-01 16:18:55 +0200

    eswrdtfnjmk,

--- Assets/Scripts/FERNANDO/Systems/GarageCanvas.cs
+++ Assets/Scripts/FERNANDO/Systems/GarageCanvas.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEngine;
 using DG.Tweening;
+using UnityEngine.SceneManagement;
 
 public class GarageCanvas : MonoBehaviour
 {
@@ -30,11 +31,20 @@ public class GarageCanvas : MonoBehaviour
         gM.playersReady++;
         if (gM.playersReady < 2)
         {
-            gM.LoadNewScene(4);
+            gM.LoadNewScene(2);
             return;
         }
 
         gM.LoadNewScene(0);
+    }
+
+    public void ClickOneButton ()
+    {
+        SceneManager.LoadScene("GarajePl1 1");
+    }
+    public void ClicktwoButton ()
+    {
+        SceneManager.LoadScene("FER");
     }
 
     private void OnDisable()
